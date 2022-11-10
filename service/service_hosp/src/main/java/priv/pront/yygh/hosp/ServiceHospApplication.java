569108f160c44cf310b4409ee383dbaa53f294f6
唐豪 <a2233192735>
2022-11-10 22:46:39 +0800

    第三次提交，接口与配置文件

---
+++ service/service_hosp/src/main/java/priv/pront/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,16 @@
+package priv.pront.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: pront
+ * @Time:2022-11-10 22:43
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
