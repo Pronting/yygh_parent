c181975fa70a448e995c2613dcd12c8627860a1d
pronting <2233192735@qq.com>
2022-11-18 10:58:18 +0800

    解决了前后端跨域问题

--- service/service_cmn/src/main/java/priv/pront/yygh/cmn/controller/DictController.java
+++ service/service_cmn/src/main/java/priv/pront/yygh/cmn/controller/DictController.java
@@ -3,10 +3,7 @@ package priv.pront.yygh.cmn.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import priv.pront.yygh.cmn.service.DictService;
 import priv.pront.yygh.common.result.Result;
 import priv.pront.yygh.model.cmn.Dict;
@@ -21,6 +18,7 @@ import java.util.List;
 @Api(value="数据字典接口")
 @RestController
 @RequestMapping("/admin/cmn/dict")
+@CrossOrigin
 public class DictController {
 
     @Autowired
