b2d6c68ef992d0548e2590956146a0baccfc665a
pronting <2233192735@qq.com>
2022-11-28 11:50:02 +0800

    Nacos管理服务

--- service/service_cmn/src/main/java/priv/pront/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/priv/pront/yygh/cmn/ServiceCmnApplication.java
@@ -2,6 +2,7 @@ package priv.pront.yygh.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Component;
  */
 @SpringBootApplication
 @ComponentScan("priv.pront")
+@EnableDiscoveryClient
 public class ServiceCmnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class, args);
     }
 }
+
--- service/service_hosp/src/main/java/priv/pront/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/priv/pront/yygh/hosp/ServiceHospApplication.java
@@ -2,6 +2,7 @@ package priv.pront.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan(basePackages = "priv.pront")
+@EnableDiscoveryClient
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
