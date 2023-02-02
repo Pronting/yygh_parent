a33b9d3c59b354ec50bf7f386938d163c81c9fa5
pronting <2233192735@qq.com>
2023-02-02 15:30:04 +0800

    添加了分页插件

--- service/service_user/src/main/java/priv/pront/yygh/user/config/UserConfig.java
+++ service/service_user/src/main/java/priv/pront/yygh/user/config/UserConfig.java
@@ -1,6 +1,8 @@
 package priv.pront.yygh.user.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,4 +13,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan(basePackages = "priv.pront.yygh.user.mapper")
 public class UserConfig {
+
+    /**
+     * 分页插件
+     * @return 插件对象
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
 }
